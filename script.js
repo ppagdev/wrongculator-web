79c5b58167519b5050ee89521c6a8c988fe30f5b
Ppag <ppagdev@tuta.com>
2024-02-06 19:32:39 +0000

    added some functions

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function appendToDisplay(arg) {
     var display = document.getElementById('display');
-    //display.innerText += arg.value;
+    display.innerText += arg.value;
+}
+
+function equals() {
+    var display = document.getElementById('display');
+    var result = math.evaluate(display.innerText);
+    display.innerText = result.toString();
+}
+
+function clearText() {
+    var display = document.getElementById('display');
     display.innerText = "";
 }
